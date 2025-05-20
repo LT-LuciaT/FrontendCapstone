7dab50428e77ba1a044b73f3528ff7ad2b16c3ca
LT-LuciaT <lucia.trombin98@gmail.com>
2025-05-20 16:57:26 +0200

    1

--- src/component/MyNavBar.jsx
+++ src/component/MyNavBar.jsx
@@ -6,13 +6,16 @@ const MyNavBar = () => {
       {/* Navbar per mobile (orizzontale) */}
       <Navbar bg="light" expand="md" className="d-md-none">
         <Container>
-          <Navbar.Brand href="#home">La Mia App</Navbar.Brand>
+          <Navbar.Brand href="#home" className="">
+            La Mia App
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar" />
           <Navbar.Collapse id="responsive-navbar">
-            <Nav className="me-auto">
+            <Nav className="me-auto ">
               <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Prezzi</Nav.Link>
+              <Nav.Link href="#categories">Categories</Nav.Link>
+              <Nav.Link href="#mymoods">MyMoods</Nav.Link>
+              <Nav.Link href="#news">News</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -20,14 +23,15 @@ const MyNavBar = () => {
 
       {/* Navbar per desktop (verticale) */}
       <Navbar bg="light" className="d-none d-md-flex vh-100 navbar-vertical">
-        <Container fluid className="flex-column h-100">
-          <Navbar.Brand href="#home" className="mb-4">
+        <Container fluid className="flex-column h-100 p-0">
+          <Navbar.Brand href="#home" className=" mb-4 ps-3">
             La Mia App
           </Navbar.Brand>
           <Nav className="flex-column w-100">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Prezzi</Nav.Link>
+            <Nav.Link href="#categories">Categories</Nav.Link>
+            <Nav.Link href="#mymoods">MyMoods</Nav.Link>
+            <Nav.Link href="#news">News</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
