1d175297ebbc89ac258ab8bce06325e3b602ecec
LT-LuciaT <lucia.trombin98@gmail.com>
2025-05-26 16:03:32 +0200

    modifiche + searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MyNavBar from "./component/MyNavBar";
+import SearchBar from "./component/SearchBar";
 
 function App() {
   return (
     <>
       <MyNavBar />
+      <SearchBar />
     </>
   );
 }
